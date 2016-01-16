adc286424c1a059899a6d76a96f12f25d989b05a
Natalie Nygaard <natalie42@me.com>
2016-01-16 14:15:08 -0800

    see current score, camera follows player

---
+++ ThereIsNeverEnough/Assets/scoreText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scoreText : MonoBehaviour {
+
+	Text scoreBox;
+	int currentScore;
+
+	void Start () {
+		scoreBox = GetComponent <Text> ();
+		currentScore = 0;
+		scoreBox.text = "People Evacuated: " + currentScore;
+
+	}
+
+	void Update () {
+		currentScore = FindObjectOfType<MainBase> ().Score;
+		scoreBox.text = "People Evacuated: " + currentScore;
+	}
+}
