4858652dd8f7e8bf8e7d7ec0d438d28c54dcac65
George Wang <georgetw1108@gmail.com>
2016-01-16 11:08:18 -0800

    Added road object and prefab. Modified Action.cs to have tabs.

--- ThereIsNeverEnough/Assets/Player/Action.cs
+++ ThereIsNeverEnough/Assets/Player/Action.cs
@@ -2,45 +2,46 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Action : MonoBehaviour {
+public class Action : MonoBehaviour
+{
+	public Vector2 direction { get; set; }
 
-    public Vector2 direction { get; set; }
+	[SerializeField]
+	protected float speed;
 
-    [SerializeField]
-    protected float speed;
+	[SerializeField]
+	protected float accel;
 
-    [SerializeField]
-    protected float accel;
-
-    Rigidbody2D rigid;
-    MainBase mainBase;
-    void Awake()
-    {
-        rigid = GetComponent<Rigidbody2D>();
-    }
-
-    void Start()
-    {
-        mainBase = FindObjectOfType<MainBase>();
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        rigid.velocity = Vector2.MoveTowards(rigid.velocity, speed * direction.normalized, accel * Time.fixedDeltaTime);
+	Rigidbody2D rigid;
+	MainBase mainBase;
+	void Awake()
+	{
+		rigid = GetComponent<Rigidbody2D>();
 	}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag(Tags.people))
-        {
-            People people = other.GetComponent<People>();
-            people.Active = true;
-            people.AddRoad(mainBase.transform);
-        }
-    }
+	void Start()
+	{
+		mainBase = FindObjectOfType<MainBase>();
+	}
 
-    public TileLocation location()
-    {
-        return new TileLocation(Mathf.RoundToInt(this.transform.position.x), Mathf.RoundToInt(this.transform.position.y));
-    }
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		rigid.velocity = Vector2.MoveTowards(rigid.velocity, speed * direction.normalized, accel * Time.fixedDeltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag(Tags.people))
+		{
+			People people = other.GetComponent<People>();
+			people.Active = true;
+			people.AddRoad(mainBase.transform);
+		}
+	}
+
+	public TileLocation location()
+	{
+		return new TileLocation(Mathf.RoundToInt(this.transform.position.x), Mathf.RoundToInt(this.transform.position.y));
+	}
 }
