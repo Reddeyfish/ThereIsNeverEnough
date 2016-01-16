e6bc2bf3ee3652df0bfc22f9f156eb55d4532317
Reddeyfish <edrichd@uci.edu>
2016-01-15 20:09:09 -0800

    add player

---
+++ ThereIsNeverEnough/Assets/Player/Action.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Action : MonoBehaviour {
+
+    public Vector2 direction { get; set; }
+
+    [SerializeField]
+    protected float speed;
+
+    [SerializeField]
+    protected float accel;
+
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rigid.velocity = Vector2.MoveTowards(rigid.velocity, speed * direction.normalized, accel * Time.fixedDeltaTime);
+	}
+}
---
+++ ThereIsNeverEnough/Assets/Player/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//reads input and sends it to the action script
+
+[RequireComponent(typeof(Action))]
+public class PlayerInput : MonoBehaviour {
+
+    Action action;
+
+	// Use this for initialization
+	void Awake () {
+        action = GetComponent<Action>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        action.direction = direction;
+	}
+}
