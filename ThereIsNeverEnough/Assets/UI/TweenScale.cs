6039c020097c58d0c97fc326462220d0583bd295
George Wang <georgetw1108@gmail.com>
2016-01-16 21:47:52 -0800

    Added scaling buttons.

--- ThereIsNeverEnough/Assets/UI/TweenScale.cs
+++ ThereIsNeverEnough/Assets/UI/TweenScale.cs
@@ -7,8 +7,17 @@ public class TweenScale : MonoBehaviour {
 	public float Speed = 1.0f;
 	public iTween.EaseType EaseType = iTween.EaseType.easeInOutQuad;
 	public iTween.LoopType LoopType = iTween.LoopType.none;
+	public bool OnStart = true;
 
 	void Start () {
+		if (OnStart)
+		{
+			Scale(gameObject);
+		}
+	}
+
+	public void Scale(GameObject gameObject)
+	{
 		iTween.ScaleTo(gameObject, iTween.Hash("scale", FinalScale, "speed", Speed, "easetype", EaseType, "looptype", LoopType));
 	}
 }
