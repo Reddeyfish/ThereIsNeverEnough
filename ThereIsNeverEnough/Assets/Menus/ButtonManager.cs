a6460b76db7deb81ebd1c7f378d283af0e59eb34
Natalie Nygaard <natalie42@me.com>
2016-01-16 19:43:26 -0800

    game of roads menu

---
+++ ThereIsNeverEnough/Assets/Menus/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+	public GameObject main;
+	public GameObject credits;
+
+	void Start() {
+		main.SetActive (true);
+		credits.SetActive (false);
+	}
+
+	public void LoadGame() {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void Credits() {
+		if (main.activeSelf) {
+			main.SetActive (false);
+			credits.SetActive (true);
+		} else {
+			main.SetActive (true);
+			credits.SetActive (false);
+		}
+
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
