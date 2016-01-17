320d9dd004c756337bbc2732d46a83ae0e688520
George Wang <georgetw1108@gmail.com>
2016-01-16 21:33:13 -0800

    Added a bunch of shit. Scale tween. Pos tween. ItweenLine.

---
+++ ThereIsNeverEnough/Assets/UI/TweenScale.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenScale : MonoBehaviour {
+
+	public Vector3 FinalScale = Vector3.one;
+	public float Speed = 1.0f;
+	public iTween.EaseType EaseType = iTween.EaseType.easeInOutQuad;
+	public iTween.LoopType LoopType = iTween.LoopType.none;
+
+	void Start () {
+		iTween.ScaleTo(gameObject, iTween.Hash("scale", FinalScale, "speed", Speed, "easetype", EaseType, "looptype", LoopType));
+	}
+}
