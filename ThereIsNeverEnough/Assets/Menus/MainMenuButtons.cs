48943a4aec1677d65bcd3f8c2e1f1c13e5fa6c6e
George Wang <georgetw1108@gmail.com>
2016-01-16 19:10:42 -0800

    Added itween plugin. Added credits screen. Added main menu. Made the Main scene the scene that the standalone build links to.

---
+++ ThereIsNeverEnough/Assets/Menus/MainMenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Contains all the functions necessary for the main menu to work
+/// </summary>
+public class MainMenuButtons : MonoBehaviour {
+
+	public void LoadScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void QuitApp()
+	{
+		Application.Quit();
+	}
+}
