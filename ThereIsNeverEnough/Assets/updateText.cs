d37e4eac2502509ae913b8631e5e30f35522f59a
Tony Dinh <tonydinh@dhcp-v035-185.mobile.uci.edu>
2016-01-17 23:28:49 -0800

    sfix update

--- ThereIsNeverEnough/Assets/updateText.cs
+++ ThereIsNeverEnough/Assets/updateText.cs
@@ -6,17 +6,33 @@ public class updateText : MonoBehaviour
 {
 
 	Text updateBox;
+	int minute = 0, time = 0;
 
 	// Use this for initialization
 	void Start () 
 	{
 		updateBox = GetComponent <Text> ();
-		updateBox.text = "Time: " + (int) Time.timeSinceLevelLoad + " sec";
+		updateBox.text = "Time: ";
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
-		updateBox.text = "Time: " + (int) Time.timeSinceLevelLoad + " sec";
+		time = (int)Time.time;
+		if (time < 60) 
+		{
+			updateBox.text = "Time: " + time + " sec";
+		} 
+		else 
+		{
+			minute = (int)Time.time / 60;
+			time = time - (minute * 60);
+			updateBox.text = "Time: " + minute + " min ";
+			if (time != 0) 
+			{
+				updateBox.text += time + " sec";
+			}
+		}
+
 	}
 }
