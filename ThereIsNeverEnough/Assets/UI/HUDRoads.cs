fcf64578dfafe0d57d9870fc54a07980b3e1d72a
George Wang <georgetw1108@gmail.com>
2016-01-17 16:00:18 -0800

    Made the HUD show the currently selected road.

---
+++ ThereIsNeverEnough/Assets/UI/HUDRoads.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDRoads : MonoBehaviour {
+
+	public Action PlayerAction;
+	public Sprite[] PossibleRoadSprites;
+	public Image HUDSprite;
+
+	private int selectedRoad = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (PlayerAction.SelectedRoadTiles != selectedRoad)
+		{
+			selectedRoad = PlayerAction.SelectedRoadTiles;
+			HUDSprite.sprite = PossibleRoadSprites[selectedRoad];
+		}
+	}
+}
