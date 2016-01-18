312f7698e450678e3bc4d5da2ec4c997e4769037
Reddeyfish <edrichd@uci.edu>
2016-01-17 16:18:48 -0800

    added music

---
+++ ThereIsNeverEnough/Assets/Music/MusicSingleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicSingleton : MonoBehaviour {
+
+    static MusicSingleton self;
+
+	// Use this for initialization
+	void Awake () {
+        if (self != null && self != this)
+            Destroy(this.gameObject);
+        else
+        {
+            self = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+	}
+}
