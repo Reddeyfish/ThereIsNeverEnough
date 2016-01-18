27f1139065e76fa82457ded93134287280876594
Natalie Nygaard <natalie42@me.com>
2016-01-17 23:29:24 -0800

    added instructions to menu, fires appear slightly less often

--- ThereIsNeverEnough/Assets/Menus/ButtonManager.cs
+++ ThereIsNeverEnough/Assets/Menus/ButtonManager.cs
@@ -5,10 +5,12 @@ using System.Collections;
 public class ButtonManager : MonoBehaviour {
 	public GameObject main;
 	public GameObject credits;
+	public GameObject tutorial;
 
 	void Start() {
 		main.SetActive (true);
 		credits.SetActive (false);
+		tutorial.SetActive (false);
 	}
 
 	public void LoadGame() {
@@ -22,6 +24,17 @@ public class ButtonManager : MonoBehaviour {
 		} else {
 			main.SetActive (true);
 			credits.SetActive (false);
+		}
+
+	}
+
+	public void Tutorial() {
+		if (main.activeSelf) {
+			main.SetActive (false);
+			tutorial.SetActive (true);
+		} else {
+			main.SetActive (true);
+			tutorial.SetActive (false);
 		}
 
 	}
